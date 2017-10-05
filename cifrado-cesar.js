f61b60b6dc37f0978236e68b52c57807d58d660a
katherineRamirez <32302585+katherineRamirez@users.noreply.github.com>
2017-10-05 15:07:44 -0300

    Add files via upload

---
+++ cifrado-cesar.js
@@ -0,0 +1,15 @@
+    function cipher(){ //creando una función para cifrar la frase ingresada
+    var frase = prompt('Ingrese una frase con mayúscula: ');
+    var respuesta = frase;
+    for(var i = 0; i < respuesta.length; i++){
+    var digitosNoValidos = 0123456789; //la frase debe contener sólo números
+    if(digitosNoValidos.indexOf == 'number'){
+      console.log(prompt('Ingrese una frase válida: '));
+      }else if(respuesta !== digitosNoValidos){ 
+    }
+    }
+  }  
+    function decipher(){//creando una función para decifrar la frase ingresada en la función cipher
+    respuesta.charCodeAt(0); //usar fórmula para sacar el código ASCII de una letra
+    
+    }
